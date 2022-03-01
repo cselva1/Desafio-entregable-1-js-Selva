a8643692285eb3ba16dd4392001ecd0ea3abd57b
cselva1 <selvacristian.94@gmail.com>
2022-02-28 23:56:54 -0300

    Se agregaron ciclos

---
+++ ciclo.js
@@ -0,0 +1,9 @@
+var dias = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"];
+
+var numeroDias = dias.length -1;
+var i=0;
+
+while (i<= numeroDias){
+    document.write(dias [i] + "<br>");
+    i++;
+}
